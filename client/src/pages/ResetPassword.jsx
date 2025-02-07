fcca3aea9ecb7be64eff4b864e5f0f12b5cd43f1
Arsh <arshahmad235@gmail.com>
2025-02-07 18:54:07 +0530

    Reset password added

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const ResetPassword = () => {
-   return <div>ResetPassword</div>;
+   return (
+      <div>
+         <div className="flex justify-center items-center h-screen">
+            <div className="w-full max-w-md">
+               <h1 className="text-2xl font-bold mb-4">Reset Password</h1>
+               <form className="space-y-4">
+                  <div className="space-y-2">
+                     <label
+                        htmlFor="password"
+                        className="block text-sm font-medium text-gray-700"
+                     >
+                        Password
+                     </label>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default ResetPassword;
